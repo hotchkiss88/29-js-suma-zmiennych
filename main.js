b64f7786860d3e9730aeaf2813a904ab336ed4db
hotchkiss88 <al-sowa@hotmail.com>
2021-08-30 22:44:30 +0200

    29-js-suma-zmiennych

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstNumber = 10;
+let secondNumber = 20;
+
+function myFunction(number1, number2) {
+    let sum = number1 + number2;
+    return sum;
+}
+myFunction(firstNumber, secondNumber);
+
+console.log(myFunction(firstNumber, secondNumber));
